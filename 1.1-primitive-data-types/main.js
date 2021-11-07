5a19adf9f736d52f2a1d8b1bba09aceb1030659c
Emanuel Yaish <emanuelyaish@gmail.com>
2021-11-07 15:35:47 +0200

    submit 1.1 primitive data type

---
+++ 1.1-primitive-data-types/main.js
@@ -0,0 +1,23 @@
+// 1. Create a variable that’s value holds a string.
+let myString = "this is a string";
+// 2. Create a variable that its value holds a number.
+let myNumber = 37;
+// 3. Create a variable that its value holds an object.
+let myObject = new Object();
+// 4. Create a variable that its value holds a boolean.
+let myBoolean = true;
+// 5. Create a variable that its value holds null.
+let myNull = null;
+// 5. Create a variable that its value is undefined.
+let myUndefiened = undefined;
+// 6. Create a variable that its value holds a symbol.
+let mySymbol = Symbol("mySymbol");
+// Console log the types of your variables.
+
+console.log(typeof myString);
+console.log(typeof myNumber);
+console.log(typeof myObject);
+console.log(typeof myBoolean);
+console.log(typeof myNull);
+console.log(typeof myUndefiened);
+console.log(typeof mySymbol);
