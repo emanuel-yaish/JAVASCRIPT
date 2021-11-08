3b484a8055a45ad418d0b24a5d48d72fac9c3d7f
Emanuel Yaish <emanuelyaish@gmail.com>
2021-11-08 20:39:29 +0200

    submit 4.5 leap year

---
+++ 4.5-leap-year/main.js
@@ -0,0 +1,19 @@
+function leapYear(year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0 && year % 400 !== 0) {
+      console.log(`"This is not a leap year.`);
+      return;
+    }
+    console.log(`It is indeed a leap year`);
+    return;
+  }
+
+  console.log(`"This is not a leap year.`);
+}
+
+leapYear(2012);
+leapYear(2100);
+leapYear(2400);
+leapYear(2018);
+leapYear(2020);
+leapYear(4800);
