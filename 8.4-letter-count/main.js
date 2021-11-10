88ae93078415bdb13b7cba3eeced52c29d8a356a
Emanuel Yaish <emanuelyaish@gmail.com>
2021-11-10 18:20:15 +0200

    submit 8.4 letter count with extra

--- 8.4-letter-count/main.js
+++ 8.4-letter-count/main.js
@@ -1,4 +1,6 @@
 const array = ["Hello", "Good Day", "Your Welcome", "hotdog", "hamburgers"];
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const counterArray = Array(26).fill(0);
 
 function letterCount(array) {
   letters = {};
@@ -18,4 +20,27 @@ function letterCount(array) {
   return letters;
 }
 
-console.log(letterCount(array));
+function letterCountArray(array) {
+  letters = {};
+
+  for (word of array) {
+    word = word.replace(/\s/g, "");
+    console.log(word);
+    for (let index = 0; index < word.length; index++) {
+      console.log(word[index].toUpperCase());
+      letter = word[index].toUpperCase();
+      counterArray[alphabet.indexOf(letter)]++;
+    }
+  }
+  console.log(counterArray);
+  return letters;
+}
+
+// extra //
+function getMaxValue() {
+  let index = counterArray.indexOf(Math.max(...counterArray));
+  return alphabet[index];
+}
+
+letterCountArray(array);
+console.log(getMaxValue(counterArray).toLowerCase());
