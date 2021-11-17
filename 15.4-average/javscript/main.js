d0dc9f086392c886e7b69357fc39bfcf285fa071
Emanuel Yaish <emanuelyaish@gmail.com>
2021-11-17 15:41:52 +0200

    submit 15.4 average fix var to let

--- 15.4-average/javscript/main.js
+++ 15.4-average/javscript/main.js
@@ -1,6 +1,6 @@
 function calcAverage(arr) {
-  var sum = 0;
-  for (var i = 0; i < arr.length; i++) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
   return sum;
