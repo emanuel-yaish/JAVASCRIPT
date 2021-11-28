b70ffa067be2b12cdfd86a34d8980bb361c7ee29
Emanuel Yaish <emanuelyaish@gmail.com>
2021-11-28 14:58:42 +0200

    submit 19.2

---
+++ 18.4-simley/sctipts/main.js
@@ -0,0 +1,13 @@
+const button = document.querySelector("#smileys-submit");
+const submitBtnEl = document.querySelector("#smileys-input");
+// const textInputEl = document.querySelectorAll("input")[0].value;
+
+const buttonInput = document.querySelector("[type='text']");
+console.log(buttonInput);
+
+// console.log(submitBtnEl, textInputEl);
+const smileysContainer = document.createElement("div");
+
+document.addEventListener("click", function () {
+  console.log("clicked");
+});
