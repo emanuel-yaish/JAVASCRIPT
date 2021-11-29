1f6cf4789c5b99e9b11def1bec2ed5e694abd0a7
Emanuel Yaish <emanuelyaish@gmail.com>
2021-11-30 00:30:10 +0200

    fix

--- 21.1-age/scrips/index.js
+++ 21.1-age/scrips/index.js
@@ -6,6 +6,4 @@ ageBtn.addEventListener("click", () => {
   ageValue.value >= 18
     ? (checkAgeMessage.innerHTML = "you can drink appear")
     : (checkAgeMessage.innerHTML = "you’re too young");
-
-  console.log(ageValue.value);
 });
