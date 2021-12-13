87aa4dce9975f831a5ccabb2168d7f55fd6612a3
Emanuel Yaish <emanuelyaish@gmail.com>
2021-12-13 15:35:53 +0200

    promises

--- 31.1-star-wars/scripts/index.js
+++ 31.1-star-wars/scripts/index.js
@@ -6,29 +6,63 @@
 //   planetPopulation,
 // };
 
-async function getCharacters() {
+async function getCharacters1() {
   try {
     const data = await (await fetch("https://swapi.dev/api/films/1")).json();
     console.log("Success:", data.characters);
     const charactersApiArray = [];
     for (let index = 0; index < 10; index++) {
-      charactersApiArray.push(
-        await (await fetch(data.characters[index])).json()
-      );
+      charactersApiArray.push(fetch(data.characters[index]));
     }
-    console.log(charactersApiArray);
-    const characters = Promise.all(charactersApiArray);
-    console.log(characters);
-    // characters.forEach((character) => {
-    //   console.log(character);
-    // });
-    console.log("Success:", data);
+    console.log("charactersApiArray", charactersApiArray);
+    const charactersPromise = await Promise.all(charactersApiArray);
+    const charactersjsonPromiseArray = charactersPromise.map(
+      (charcterPromise) => charcterPromise.json()
+    );
+    console.log(charactersjsonPromiseArray);
+    const characters = await Promise.all(charactersjsonPromiseArray);
+
+    console.log("Success:", characters);
   } catch (error) {
     console.error("Error:", error);
   }
 }
 
-getCharacters();
+getCharacters1();
+
+// CODE WITH MORDI
+async function getCharacters() {
+  try {
+    const data = await (await fetch("https://swapi.dev/api/films/1")).json();
+    console.log("Success:", data.characters);
+    // const charactersApiArray =
+
+    // console.log(charactersApiArray);
+    const responseArray = await Promise.all(
+      data.characters.slice(0, 10).map((e) => fetch(e))
+    );
+    console.log("responseArray", responseArray);
+    const jsonPromiseArray = responseArray.map((response) => response.json());
+    console.log("jsonPromiseArray", jsonPromiseArray);
+
+    const characters = await Promise.all(jsonPromiseArray);
+
+    // const characters = await Promise.all(
+    //   (
+    //     await Promise.all(data.characters.slice(0, 10).map((e) => fetch(e)))
+    //   ).json()
+    // );
+    console.log("characters", characters);
+    // characters.forEach((character) => {
+    //   console.log(character);
+    // });
+    // console.log("Success:", data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+// getCharacters();
 
 // const titleElement = document.querySelector(".joke-title");
 // const jokeElement = document.querySelector(".joke");
